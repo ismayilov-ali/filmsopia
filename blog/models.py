d9137484cde667292e3374a5576633618860bfab
ismayilovali0 <ismayilovali@protonmail.com>
2026-01-07 15:07:16 +0400

    yenilikler

--- blog/models.py
+++ blog/models.py
@@ -48,3 +48,10 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.post.title}'
